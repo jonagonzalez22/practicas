803ba9f98d7bef8b74877a712cff450e2a88d283
Jonathan González <yonathanmf@gmail.com>
2024-07-22 13:08:51 -0300

    Jonathan test refactor added

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,10 +1,44 @@
+import { useState, useEffect } from 'react';
+import { Items } from './utils';
 const Test = () => {
+  const [categorySelected, setCategorySelected] = useState<string>(
+    Items[0].category
+  );
 
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className="mainContainer">
+      <nav className="headerMenu">
+        {Items.map((itemCategory) => (
+          <span
+            className={
+              itemCategory.category === categorySelected ? 'selected' : ''
+            }
+            key={itemCategory.category}
+            onClick={() => {
+              setCategorySelected(itemCategory.category);
+            }}
+          >
+            {itemCategory.category}
+          </span>
+        ))}
+      </nav>
 
-export default Test
+      {Items.filter((item) => item.category === categorySelected).flatMap(
+        (categoryItem) =>
+          categoryItem.items.map((item) => (
+            <article className="itemsContainer" key={item.name}>
+              <div className="imageContainer">
+                <img src={item.img} className="imgDescription" />
+              </div>
+              <div className="itemContainerDescriptions">
+                <span>{item.name}</span>
+                <div>{item.description}</div>
+              </div>
+            </article>
+          ))
+      )}
+    </div>
+  );
+};
+
+export default Test;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import Test from "../components/Test"
 const inter = Inter({ subsets: ["latin"] })
 
 export default function Home() {
-  return <main className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}>
+  return <main className={`bg-black flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}>
     <Test />
   </main>
 }
